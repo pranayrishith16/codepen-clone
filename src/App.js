4a99251969f7dda9405620a75d29bfbd512375b1
pranay <pranayrishith007@gmail.com>
2022-03-04 20:06:20 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import Editor from './components/Editor';
+import React, { useState, useEffect } from 'react'
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
+
+  const [html, setHtml] = useLocalStorage('html','');
+  const [css, setCss] = useLocalStorage('css','');
+  const [js, setJs] = useLocalStorage('js','');
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      setSrcDoc(
+    `
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>
+    `
+    )
+    },500)
+
+    return () => clearTimeout(timeOut)
+  }, [html,css,js])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="pane top-pane">
+      <Editor 
+        language="xml"
+        displayName="HTML" value={html} onChange={setHtml} />  
+      <Editor 
+      language="css"
+      displayName="CSS" value={css} onChange={setCss}/>  
+      <Editor 
+      language="javascript"
+      displayName="JS" value={js} onChange={setJs}/>  
     </div>
+    <div className="pane ">
+      <iframe 
+        srcDoc={srcDoc}
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />  
+    </div>
+    </>
   );
 }
 
